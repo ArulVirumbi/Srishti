8c161b95404e13c525bd3343f03c1b7baa5f099b
Arul Virumbi <arul.virumbi@gmail.com>
2021-08-01 11:18:42 +0530

    completed the website

---
+++ header.php
@@ -0,0 +1,21 @@
+<section id="hero" class="d-flex flex-column align-items-center justify-content-center">
+    <div class="row" style="margin: auto; padding-top: 30px;">
+      <div class="col-lg-12 d-flex align-items-center justify-content-center">
+        <img class="image img-fluid" src="assets/img/hero1.png">
+      </div>
+      <div class="col-lg-12 d-flex align-items-center justify-content-center">
+          <h2>IEEE STUDENTS CHAPTER 12951</h2>
+      </div>
+      
+      <div class="col-lg-12 d-flex align-items-center justify-content-center">
+        <a href="#header2" class="btn-get-started scrollto"><i class="bi bi-chevron-double-down"></i></a>
+      </div>
+      <!--<div class="col-lg-3">
+        <img class="image" src="assets/img/cyan-srishti.png">
+      </div>
+      <div class="col-lg-9">
+        <img class="image1" src="assets/img/SRiSHTi 2k21 logo part-cyan-1.png">
+      </div> -->
+    </div>
+    
+  </section>
